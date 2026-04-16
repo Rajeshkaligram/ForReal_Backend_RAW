dcd3e5ecf0fedae49f851f7d7890dbf122f06c82
Rajeshkaligram <rajeshkaligram@gmail.com>
2026-04-17 00:13:25 +0530

    Add page_section table migration

---
+++ database/migrations/2024_04_16_000006_create_page_section_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('page_section', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->timestamps();
+        });
+
+        // Insert default sections
+        DB::table('page_section')->insert([
+            ['id' => 1, 'name' => 'SEO', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'name' => 'First', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'name' => 'Second', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'name' => 'Third', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('page_section');
+    }
+};
