ee953589ff64a1927b284e00c161e0092f9dc262
Rajeshkaligram <rajeshkaligram@gmail.com>
2026-04-17 00:24:37 +0530

    Add default page_content data

--- database/migrations/2024_04_16_000004_create_page_content_table.php
+++ database/migrations/2024_04_16_000004_create_page_content_table.php
@@ -21,6 +21,34 @@ return new class extends Migration
             $table->text('content')->nullable();
             $table->timestamps();
         });
+
+        // Insert default page content for home page
+        DB::table('page_content')->insert([
+            // SEO Section (section_id=1)
+            ['page_id' => 1, 'section_id' => 1, 'field_type' => 'text', 'field_name' => 'Meta Title', 'content' => 'Home', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 1, 'field_type' => 'textarea', 'field_name' => 'Meta Description', 'content' => 'Home', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 1, 'field_type' => 'text', 'field_name' => 'Meta Keys', 'content' => 'Home', 'created_at' => now(), 'updated_at' => now()],
+            // First Section (section_id=2)
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'text', 'field_name' => 'Title Text', 'content' => 'IMPRESS THEM ALL,', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'image', 'field_name' => 'Image 1', 'content' => 'uploads/cms/1510268908__rentasuit.png', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'text', 'field_name' => 'Text 1', 'content' => 'QUALITY. STYLISH. ACCESSIBLE. PROFESSIONAL.', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'text', 'field_name' => 'Button Text 1', 'content' => 'I want to RENT', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'text', 'field_name' => 'Button Text 2', 'content' => 'I want to POST', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 2, 'field_type' => 'text', 'field_name' => 'Subtitle', 'content' => 'Start your journey today', 'created_at' => now(), 'updated_at' => now()],
+            // Second Section (section_id=3)
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'image', 'field_name' => 'Image 1', 'content' => 'uploads/cms/1510269790__section-2-img-wrapper.png', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'image', 'field_name' => 'Image 2', 'content' => 'uploads/cms/1510269790__section-2-img.png', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Background Text', 'content' => 'welcome', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Title Text 1', 'content' => 'renting a suit', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Title Text 2', 'content' => 'at an affordable price', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Text 1', 'content' => 'Now you can rent or post the latest brand and garments for less.', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Text 2', 'content' => 'Rent used quality clothes for business, special occasions or events.', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 3, 'field_type' => 'text', 'field_name' => 'Text 3', 'content' => 'Why not be part of a supportive community while making someone else happy at the same time!', 'created_at' => now(), 'updated_at' => now()],
+            // Third Section (section_id=4)
+            ['page_id' => 1, 'section_id' => 4, 'field_type' => 'text', 'field_name' => 'Background Text', 'content' => 'The Process', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 4, 'field_type' => 'text', 'field_name' => 'Title Text 1', 'content' => 'rentals that suit you', 'created_at' => now(), 'updated_at' => now()],
+            ['page_id' => 1, 'section_id' => 4, 'field_type' => 'text', 'field_name' => 'Title Text 2', 'content' => 'is finally simple', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
