fbc75c94587620c86fc6224c278404bc1eb9309c
Rajeshkaligram <rajeshkaligram@gmail.com>
2026-04-18 20:03:07 +0530

    Remove Resend package, revert mail config

--- config/mail.php
+++ config/mail.php
@@ -2,39 +2,24 @@
 
 return [
 
-    'driver' => env('MAIL_DRIVER', env('MAIL_MAILER', 'resend')),
+    'driver' => env('MAIL_DRIVER', 'smtp'),
 
-    'mailer' => [
-        'resend' => [
-            'transport' => 'resend',
-        ],
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
-        'smtp' => [
-            'transport' => 'smtp',
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
-        ],
-
-        'log' => [
-            'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
-        ],
-
-        'array' => [
-            'transport' => 'array',
-        ],
-    ],
+    'port' => env('MAIL_PORT', 587),
 
     'from' => [
         'address' => env('MAIL_FROM', 'info@rentsuit.com'),
         'name' => env('MAIL_FROM_NAME', 'Rent A Suit'),
     ],
+
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+
+    'username' => env('MAIL_USERNAME'),
+
+    'password' => env('MAIL_PASSWORD'),
+
+    'sendmail' => '/usr/sbin/sendmail -t',
 
     'markdown' => [
         'theme' => 'default',
